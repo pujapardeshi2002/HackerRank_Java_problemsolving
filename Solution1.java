c49abb974e5353df1c9d46e9b74063d3c394a3ef
Puja Pardeshi <102475519+pujapardeshi2002@users.noreply.github.com>
2023-10-08 14:57:34 +0530

    Add files via upload

---
+++ Solution1.java
@@ -0,0 +1,28 @@
+// Java Loops
+import java.util.*;
+import java.io.*;
+import java.lang.Math;
+
+class Solution1{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            int arr[]=new int[n];
+            arr[0]=a+(int)(Math.pow(2,0)*b);
+            for(int j=1;j<n;j++)
+            {
+                arr[j]=arr[j-1]+(int)(Math.pow(2,j)*b);
+            }
+            for(int el:arr){
+                System.out.print(el+" ");
+                
+            }
+            System.out.println();
+        }
+        in.close();
+    }
+}
---
+++ Solution2.java
@@ -0,0 +1,12 @@
+# String split and join
+def split_and_join(line):
+     a = line.split(" ")
+
+     b = "-".join(a)
+
+     return(b)
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
